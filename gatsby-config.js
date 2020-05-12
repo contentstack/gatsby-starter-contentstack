e3894db67d570c520b691f86faaa2ef6257c94fc
asmit-patil <32155127+asmit-patil@users.noreply.github.com>
2020-05-12 11:44:11 +0530

    updated config options

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,9 +26,9 @@ module.exports = {
         'delivery_token': process.env.CONTENTSTACK_DELIVERY_TOKEN,
         'environment': process.env.CONTENTSTACK_ENVIRONMENT,
         // Optional: expediteBuild set this to either true or false
-        'expediteBuild': `true`,
+        'expediteBuild': true,
         // Optional: Specify true if you want to generate custom schema
-        'enableSchemaGeneration' : `true`,
+        'enableSchemaGeneration' : true,
         // Optional: Specify a different prefix for types. This is useful in cases where you have multiple instances of the plugin to be connected to different stacks.
         'type_prefix': `Contentstack`, // (default)
       },
