5cadeb1287799eccc3440241e6954f18b34505f1
Amitkanswal <amit.kanswal@contentstack.com>
2020-07-14 18:40:23 +0530

    small config changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,9 +34,9 @@ module.exports = {
         'delivery_token': process.env.CONTENTSTACK_DELIVERY_TOKEN,
         'environment': process.env.CONTENTSTACK_ENVIRONMENT,
         // Optional: expediteBuild set this to either true or false
-        'expediteBuild': false,
+        'expediteBuild': true,
         // Optional: Specify true if you want to generate custom schema
-        'enableSchemaGeneration' : false,
+        'enableSchemaGeneration' : true,
         // Optional: Specify a different prefix for types. This is useful in cases where you have multiple instances of the plugin to be connected to different stacks.
         'type_prefix': `Contentstack`, // (default)
         'downloadAssets': true
